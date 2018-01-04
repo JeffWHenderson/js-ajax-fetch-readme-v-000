b04da91ec412e4386cf36700d7ba14036ea84d4d
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-04 07:25:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '366a1451c15b84936ebf7392ac355'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
